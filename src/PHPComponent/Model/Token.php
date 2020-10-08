dfd42eb78412258c14d62dbaa0cb6b619d7bfb58
Cheng Wai Lim <s1113618@s.eduhk.hk>
2020-10-08 10:59:24 +0800

    update

--- src/PHPComponent/Model/Token.php
+++ src/PHPComponent/Model/Token.php
@@ -2,21 +2,28 @@
 class Token extends BaseModel{
     public $userID;
     public $expiredDate;
+    public $token;
 
-    public static function getDetailField(){
+    public static function getPublicField(){
         return array(
             array("key" => "userID", "type"=> BaseTypeEnum::STRING),
+            array("key" => "token", "type"=> BaseTypeEnum::STRING),
             array("key" => "expiredDate", "type"=> BaseTypeEnum::STRING),
         );
     }
-    public static function getPublicField(){
+    public static function getDetailField(){
         return array();
     }
+    
     public static function getFields($mode = BaseModel::PUBLIC){
         return self::initGetFields(self::getPublicField(), self::getDetailField(), self::getSystemField(), $mode);
     }
     public function __construct($object, $mode = BaseModel::PUBLIC){
-        parent::__construct($object);
+        parent::__construct($object, $mode);
         $this->init($object, self::getPublicField(), self::getDetailField(), self::getSystemField(), $mode);
+    }
+
+    public static function getFieldsWithType($mode = BaseModel::PUBLIC){
+        return self::initGetFieldsWithType(self::getPublicField(), self::getDetailField(), self::getSystemField(), $mode);
     }
 }
