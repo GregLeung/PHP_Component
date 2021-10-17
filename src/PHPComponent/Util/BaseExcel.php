8635b06573ffde15528a371d290016a9a894cca6
Cheng Wai Lim <s1113618@s.eduhk.hk>
2021-10-17 21:16:37 +0800

    update

--- src/PHPComponent/Util/BaseExcel.php
+++ src/PHPComponent/Util/BaseExcel.php
@@ -1,6 +1,5 @@
 <?php
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Style\Style;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class BaseExcel {
@@ -8,15 +7,7 @@ class BaseExcel {
     private $sheet = null;
     private $spreadsheet = null;
     private $previousSetRowPosition = array(1,1);
-
-    const FORMAT_GENERAL = 'General';
-    const FORMAT_TEXT = '@';
-    const FORMAT_NUMBER = '0';
-    const FORMAT_NUMBER_00 = '0.00';
-    const FORMAT_NUMBER_00_COMMA = '#,##0.00';
-    const FORMAT_PERCENTAGE = '0%';
-    const FORMAT_PERCENTAGE_00 = '0.00%';
-
+    
     function __construct() {
         $this->spreadsheet = new Spreadsheet();
         $this->sheet = $this->spreadsheet->getActiveSheet();
@@ -27,15 +18,6 @@ class BaseExcel {
         $this->currentPosition = $cell;
     }
 
-    function setCellFormat($cell, $formatCode){
-        if($formatCode != null){
-            $this->sheet->getStyle(self::convertNumberToLetter($cell[0]) . strval($cell[1]))
-                ->getNumberFormat()
-                ->setFormatCode($formatCode);
-            $this->currentPosition = $cell;
-        }
-    }
-
     function rawSetCellValue($cell, $value){
         $this->sheet->setCellValue($cell, $value);
     }
@@ -44,43 +26,25 @@ class BaseExcel {
         $this->currentPosition = $position;
     }
 
-    function checkOptionValid($valueList, $options){
-        if($options != null){
-            if(isset($options["formatList"]) && sizeof($options["formatList"]) != sizeof($valueList))
-                throw new Exception("BaseExcel::FORMAT_LENGTH_ERROR");
-        }
-    }
-
     function setRowValue($valueList, $options = null){
-        $this->checkOptionValid($valueList, $options);
-
         if(isset($options["currentPosition"]))
             $this->currentPosition = $options["currentPosition"];
-
         $this->previousSetRowPosition = $this->currentPosition;
-
         for($i = 0; $i < sizeof($valueList); $i++){
+            if(isset($options["style"]))
+                $this->setCellStyle($this->currentPosition, $options["style"]);
             $this->setCellValue($this->currentPosition, $valueList[$i]);
-            if($options != null && sizeof($options['formatList']) > 0)
-                $this->setCellFormat($this->currentPosition, $options['formatList'][$i]);
             $this->currentPosition[0] += 1;
         }
         $this->nextRow();
-        
     }
 
     function setColumnValue($valueList, $options = null){
-        $this->checkOptionValid($valueList, $options);
-
         if(isset($options["currentPosition"]))
             $this->currentPosition = $options["currentPosition"];
-
         $this->previousSetRowPosition = $this->currentPosition;
-        
         for($i = 0; $i < sizeof($valueList); $i++){
             $this->setCellValue($this->currentPosition, $valueList[$i]);
-            if($options != null && sizeof($options['formatList']) > 0)
-                $this->setCellFormat($this->currentPosition, $options['formatList'][$i]);
             $this->currentPosition[1] += 1;
         }
         $this->nextColumn();
@@ -117,6 +81,32 @@ class BaseExcel {
         foreach ($valueList as $row => $height) {
             $this->sheet->getRowDimension($row)->setRowHeight($height);
         }
+    }
+    
+    function setImage($path,$options = array()){
+        $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+        $drawing->setPath($path);
+        if(isset($options["width"]))
+            $drawing->setWidth($options["width"]);
+        if(isset($options["height"]))
+            $drawing->setHeight($options["height"]);
+        if(isset($options["offsetX"]))
+            $drawing->setOffsetX($options["offsetX"]);
+        if(isset($options["offsetY"]))
+            $drawing->setOffsetY($options["offsetY"]);
+        $drawing->setWorksheet($this->sheet);
+    }
+
+    function setColumnWidthAuto(){
+        foreach(range('A','Z') as $columnID) {
+            $this->sheet->getColumnDimension($columnID)->setAutoSize(true);
+        }
+    }
+    function setCellStyle($cell, $style){
+        $this->sheet->getStyle(self::convertNumberToLetter($cell[0]) . strval($cell[1]))->applyFromArray($style);
+    }
+    function setDefaultStyle($style){
+        $this->sheet->getDefaultStyle()->applyFromArray($style);
     }
 
     function save($path){
